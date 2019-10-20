29e23096aea87766e3a9be1a8bdf9224b270e58a
kensinwong <kensinzc@163.com>
2019-10-20 13:51:03 +0800

    update

--- time.c
+++ time.c
@@ -9,13 +9,18 @@ int main()
 
 	scanf("%d %d",&hour2,&min2);
 
-	int time1 = hour1 * 60 + min1;
+	int time2 = hour1 * 60 + min1;
 
-	int time2 = hour2 * 60 + min2;
+	int time1 = hour2 * 60 + min2;
 
 	int hour = (time1 - time2) / 60;
 
 	int min = (time1 - time2) % 60;
+	if(min<0)
+	{
+		min = 60 + min;
+		hour --;
+	}
 	
 	printf("时间差为 %d 小时 %d 分钟\n",hour,min);
 
