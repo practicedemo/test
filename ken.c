21a9da667ef2239f0a66ffc687d1e151d94e1594
kensinwong <kensinzc@163.com>
2019-10-21 15:23:27 +0800

    new

--- ken.c
+++ ken.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
-int main ()
+int main()
 {
-	printf("%d\n",7>=3+4);
+	//初始化
+	int price = 0;
+	int bill = 0;
+	//输入票面及金额
+	printf("请输入金额\n");
+	scanf("%d",&price);
+	printf("请输入票面\n");
+	scanf("%d",&bill);
+	//计算找零
+	if(price>=bill)
+	{
+		printf("找您：%d\n",bill-price);
+	}
 	return 0;
 }
