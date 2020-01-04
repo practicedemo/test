ff4c65b3f80b493ed856a882d7a2a84ad207270d
kensinwong <kensinzc@163.com>
2020-01-04 10:35:31 +0800

    202014

--- ken.c
+++ ken.c
@@ -1,30 +1,16 @@
 #include<stdio.h>
-int isPrime(int i){
-	int ret = 1;
-	int k;
-	for(k = 2; k < i-1; k++){
-		if( i%k == 0 ){
-			ret = 0;
-			break;
-		}
+int sum(int begin,int end){
+	int i,ret=0;
+	for(i=begin;i<=end;i++){
+		ret += i;
 	}
-	return ret;
+	printf("%d到%d的和为%d\n",begin,end,ret);
 }
 int main(){
-	int m,n;
-	int sum = 0;
-	int cnt = 0;
-	int i;
+	
+		sum(1,10);
+		sum(10,25);
+		sum(35,45);
 
-	scanf("%d %d",&m,&n);
-	if(m == 1) m = 2;
-	for(i = m;i<=n;i++){
-		if(isPrime(i)){
-			sum += i;
-			cnt++;
-		}
-	}
-	printf("cnt=%d sum=%d\n",cnt,sum);
-
-	return 0;
+		return 0;
 }
